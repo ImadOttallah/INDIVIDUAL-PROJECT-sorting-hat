02236fc8611f1a18ea64a5c05bfa0141b7ceba47
Imad Ottallah <imadotta@gmail.com>
2022-05-05 21:34:36 -0500

    added error message and worked on css

--- scripts/main.js
+++ scripts/main.js
@@ -20,12 +20,11 @@ const students = [
 const badStudents = [
 
 ];
-const choices = ["gryffindor", "hufflepuff", "ravenclaw", "slytherin"];
+const choices = ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"];
 const gryffindorImage = `https://cdn11.bigcommerce.com/s-ydriczk/images/stencil/1280x1280/products/88361/91122/Harry-Potter-Gryffindor-Crest-Official-wall-mounted-cardboard-cutout-buy-now-at-star__95823.1507640354.jpg?c=2?imbypass=on`;
 const hufflepuffImage = `https://cdn11.bigcommerce.com/s-ydriczk/images/stencil/1280x1280/products/88364/91134/Harry-Potter-Hufflepuff-Crest-Official-wall-mounted-cardboard-cutout-buy-now-at-star__21122.1507644096.jpg?c=2?imbypass=on`
 const ravenclawImage = `https://www.seekpng.com/png/full/184-1840811_ravenclaw-crest-harry-potter-harry-potter-ravenclaw-house.png`
 const slytherinImage = `https://cdn11.bigcommerce.com/s-ydriczk/images/stencil/1280x1280/products/88362/91127/Harry-Potter-Slytherin-Crest-Official-wall-mounted-cardboard-cutout-buy-now-at-star__31920.1507640618.jpg?c=2?imbypass=on`
-
 
 /******* UTILITY FUNCTION *******/
 const renderToDom = (divId, textToRender) => {
@@ -33,20 +32,18 @@ const renderToDom = (divId, textToRender) => {
   selectedElement.innerHTML = textToRender;
 };
 
-
 /******* STUDENTS *******/
 const studentsOnDom = (array) => {
   const student = array.filter(taco => taco.darkside === false);
-  //console.log(array)
   let domString = "";
   for (const item of student) {
   domString += `
-  <div class="card" style="width: 18rem;">
-  <img src="${item.image}" class="card-img-top" alt="...">
+  <div class="card rcorners" style="width: 18rem;">
+  <img src="${item.image}" class="card-img-top rcorners" alt="...">
   <div class="card-body" id="studentCard">
     <h5 class="card-title">${item.name}</h5>
     <p class="card-text">${item.school}</p>
-    <a href="#" class="btn btn-primary" id="expel--${item.id}" >Expel</a>
+    <a href="#" class="btn btn-primary expelBtn" id="expel--${item.id}" >Expel</a>
   </div>
 </div>`
 };
@@ -58,11 +55,11 @@ const voldOnDom = (array) => {
   const student = array.filter(taco => taco.darkside === false);
   for (const item of student) {
   domString += `
-  <div class="card" style="width: 18rem;">
-  <img src="${item.image}" class="card-img-top" alt="...">
+  <div class="card2 rcorners" style="width: 18rem;">
+  <img src="https://i1.wp.com/starbaseatlanta.com/wp-content/uploads/hp-maab21252.jpg?fit=510%2C714&ssl=1" class="card-img-top rcorners" alt="...">
   <div class="card-body" id="studentCard">
-    <h5 class="card-title">${item.name}</h5>
-    <p class="card-text">${item.school}</p>
+    <h5 class="card-title">${item.name} has joined</h5>
+    <p class="card-text">Voldermort's Army</p>
   </div>
 </div>`
 };
@@ -70,51 +67,67 @@ renderToDom("#voldContainer", domString)
 };
 
 /* ADD STUDENTS */
-function addStudent(){
-  //const choices = ["gryffindor", "hufflepuff", "ravenclaw", "slytherin"];
+function addStudent(){ 
+  const input = document.querySelector("#studentInput").value
+  if (input === "") {
+    const errorMsg = document.querySelector("#errorMessage");
+    errorMsg.innerHTML = "Please enter your name!"
+  } else {
   const randomSchool = choices[Math.floor(Math.random()*choices.length)];
   const newStudent = {
     id: students.length + 1,
     image: "",
-    name: document.querySelector("#studentInput").value,
+    name: input,
     school: randomSchool,
     darkside: false
   };
-
-  if (randomSchool === "gryffindor") {
+  if (randomSchool === "Gryffindor") {
     newStudent.image = gryffindorImage;
-  } else if (randomSchool === "ravenclaw") {
+  } else if (randomSchool === "Ravenclaw") {
     newStudent.image = ravenclawImage;
-  } else if (randomSchool === "slytherin") {
+  } else if (randomSchool === "Slytherin") {
     newStudent.image = slytherinImage;
   } else {
     newStudent.image = hufflepuffImage;
   }
-
   students.push(newStudent);
   document.querySelector("#studentInput").value="";
   studentsOnDom(students);
-  };
+  }
+};
 
+
+/* FILTER BUTTONS */
+  const filterButtons = () => {
+    const domString = `
+    <div class="navigation-btn">
+  <button id="all">All</button>
+  <button id="Gryffindor">Gryffindor</button>
+  <button id="Hufflepuff">Hufflepuff</button>
+  <button id="Ravenclaw">Ravenclaw</button>
+  <button id="Slytherin">Slytherin</button>
+    </div>
+    `;
+    renderToDom('#filterStudents', domString);
+  };
 
 
 /****** EVENT LISTENERS ******/
 const eventListeners = () => {
   document.querySelector("#filterStudents").addEventListener("click", (e) => {
-  console.log("you clicked me", e.target.id);
   if (e.target.id === "all") {
     studentsOnDom(students);
-  } else if (e.target.id === "gryffindor") {
-      const gryffindor = students.filter(taco => taco.school === "gryffindor" )
+  } else if (e.target.id === "Gryffindor") {
+      const gryffindor = students.filter(taco => taco.school === "Gryffindor" )
       studentsOnDom(gryffindor);
-    } else if (e.target.id === "hufflepuff") {
-      const hufflepuff = students.filter(taco => taco.school === "hufflepuff" )
+    } else if (e.target.id === "Hufflepuff") {
+      const hufflepuff = students.filter(taco => taco.school === "Hufflepuff" )
       studentsOnDom(hufflepuff)
-    } else if (e.target.id === "ravenclaw") {
-      const ravenclaw = students.filter(taco => taco.school === "ravenclaw" )
+    } else if (e.target.id === "Ravenclaw") {
+      const ravenclaw = students.filter(taco => taco.school === "Ravenclaw" )
       studentsOnDom(ravenclaw)
-    } else if (e.target.id === "slytherin") {
-      const slytherin = students.filter(taco => taco.school === "slytherin" )
+    } else if (e.target.id === "Slytherin") {
+      const slytherin = students.filter(taco => taco.school === "Slytherin" )
       studentsOnDom(slytherin)
     }
     
@@ -122,7 +135,7 @@ const eventListeners = () => {
 
 
 /** EXPEL TO VOLDEMORT ARMY **/
-document.querySelector("#studentContainer").addEventListener("click",(e) => {
+document.querySelector("#studentContainer").addEventListener("submit",(e) => {
   if (e.target.id) {
     const [method, id] = e.target.id.split("--");
     const index = students.findIndex(taco => taco.id === parseInt(id));
@@ -130,8 +143,6 @@ document.querySelector("#studentContainer").addEventListener("click",(e) => {
   badStudents.push(...students.splice(index, 1));
   studentsOnDom(students);
   voldOnDom(badStudents);
-  console.log(students)
-  console.log(badStudents)
   };
  });
  };
@@ -141,6 +152,7 @@ document.querySelector("#studentContainer").addEventListener("click",(e) => {
 const startApp = () => {
   studentsOnDom(students);
   voldOnDom(badStudents);
+  filterButtons();
   eventListeners();
 };
 
